4fa8fc6e1701f9c95af894585d1d2bb64296dcad
yvbbrjdr <xyyqzy@hotmail.com>
2025-09-20 17:09:11 -0700

    Update package dependencies, enhance documentation with icons for various pages, and improve source configuration for icon rendering.

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,9 +1,20 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
   baseUrl: '/',
   source: docs.toFumadocsSource(),
+  icon(iconName) {
+    if (!iconName) {
+      return;
+    }
+
+    if (iconName in icons) {
+      return createElement(icons[iconName as keyof typeof icons]);
+    }
+  },
 });
